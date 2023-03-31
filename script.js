171ad95ccbc9fd76c6f2957345a4d4fd7708d280
Piotr Ochodek <ohis@op.pl>
2023-03-31 10:25:05 +0200

    [200~I added a function that creates one array from the arrays created by the generateArrayWithRandomNumbers function

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 const generateArrayWithRandomNumbers = (howManyNumbers, min, max) => {
-  let arrNum = []
+  let arrNum = [];
 
   for (let i = 0; i < howManyNumbers; i++) {
     let randomNumber = Math.floor(Math.random() * (max - min) + min);
-    arrNum.push(randomNumber)
+    arrNum.push(randomNumber);
   }
+  const newArr = new Array(arrNum);
 
-  console.log(arrNum)
+  console.log(newArr);
+  return newArr;
 };
-generateArrayWithRandomNumbers(19,20,100);
+const arr1 = generateArrayWithRandomNumbers(19, 20, 100);
+const arr2 = generateArrayWithRandomNumbers(10, 10, 50);
+const arr3 = generateArrayWithRandomNumbers(5, 2, 30);
+const arr4 = generateArrayWithRandomNumbers(8, 45, 72);
+
+const generateArraysWithArrays = (...arr) => {
+  return [...arr];
+};
+const allArrays = generateArraysWithArrays(arr1, arr3, arr3, arr4);
+console.log(allArrays);
